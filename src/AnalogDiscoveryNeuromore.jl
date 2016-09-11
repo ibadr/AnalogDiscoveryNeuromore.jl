93959ec49c0a9da12432f5c857d48b5b94576108
Islam Badreldin <ibadreldin@ufl.edu>
2016-09-10 22:53:20 -0400

    Attempt near-real-time AnalogIn with acqmodeRecord

--- src/AnalogDiscoveryNeuromore.jl
+++ src/AnalogDiscoveryNeuromore.jl
@@ -2,21 +2,12 @@ module AnalogDiscoveryNeuromore
 
 using AnalogDiscovery
 
+include("RingBuffer.jl")
+
 const fs = 512.0
-const runForMin = 5
+const runForMin = 0.5
 const twoChannels = true
 const lowVolt = false
-
-immutable ChannelDoubleBuffer
-  chDataBuf1::Vector{Float64}
-  chDataBuf2::Vector{Float64}
-end
-
-type ChannelState
-  writeSecondBuf::Bool
-  readSecondBuf::Bool
-  waitForWriteFinish::Bool
-end
 
 function float32msg!(buffer::Vector{UInt8},ch::Int64,val::Vector{Float32})
   buffer[1:5]=(ch==1) ? "/in/0".data : "/in/1".data
@@ -53,7 +44,7 @@ function go()
   analogInFrequencySet(hdwf,fs)
   analogInBufferSizeSet(hdwf,adbufsize)
   analogInAcquisitionModeSet(hdwf,ACQMODERECORD)
-  analogInRecordLengthSet(hdwf,runForMin*60.0)
+  analogInRecordLengthSet(hdwf,0.0) # run indefinitely
 
   chVoltMin,chVoltMax,chSteps = analogInChannelRangeInfo(hdwf)
   chCount = analogInChannelCount(hdwf)
@@ -71,100 +62,91 @@ function go()
   # chData::Vector{Vector{Float64}} = [Vector{Float64}(blks*adbufsize) for i in 1:chCount]
   tBuf = Vector{Float64}(blks*adbufsize)
 
-  # Double-buffering
-  ch1dbuf::ChannelDoubleBuffer = ChannelDoubleBuffer(Vector{Float64}(adbufsize),Vector{Float64}(adbufsize))
-  ch2dbuf::ChannelDoubleBuffer = ChannelDoubleBuffer(Vector{Float64}(adbufsize),Vector{Float64}(adbufsize))
-  chState::ChannelState = ChannelState(false,false,false)
+  # RingBuffers
+  ch1buf = RingBuffer(Vector{Float64}(adbufsize),1,1)
+  ch2buf = RingBuffer(Vector{Float64}(adbufsize),1,1)
   # Single-element buffer to reinterpret Float32 value later using unsafe_load
   valBuf::Vector{Float32} = Vector{Float32}(1)
   # Buffer for OSC packets sent over UDP
   udpBuf = Array{UInt8,1}(16)
 
-  # Prefill
-  sleep(2.0)
+  # sleep(2.0)
   analogInConfigure(hdwf,false,true) # Start acquistion
-  status = analogInStatus(hdwf,false); avail,lost,corrupt = analogInStatusRecord(hdwf)
-  while avail < round(Int32,fs)
-    status = analogInStatus(hdwf,false)
-    avail,lost,corrupt = analogInStatusRecord(hdwf)
-  end
-  # # Discard the first sec for it has transients
-  # @show avail
-  # status = analogInStatus(hdwf,true); avail,lost,corrupt = analogInStatusRecord(hdwf)
-  # @show avail
-  # while avail != round(Int32,fs)
-  #   status = analogInStatus(hdwf,false); avail,lost,corrupt = analogInStatusRecord(hdwf)
-  # end
 
-  status = analogInStatus(hdwf,true); avail,lost,corrupt = analogInStatusRecord(hdwf)
-  @show (avail,lost,corrupt)
-  chState.writeSecondBuf = false
-  for ch in Int32(0):(twoChannels?Int32(chCount-1):Int32(0))
-    chdbuf = (ch==0)?ch1dbuf:ch2dbuf
-    writeBuf::Vector{Float64} = chState.writeSecondBuf?chdbuf.chDataBuf2:chdbuf.chDataBuf1
-    analogInStatusData!(writeBuf,hdwf,ch)
+  # Prefill
+  status = analogInStatus(hdwf,true)
+  while status != DWFSTATETRIGGEREDRUNNING
+    status = analogInStatus(hdwf,true)
   end
-  chState.writeSecondBuf = true
-
-  startT = Libc.time(); t = -1;
-  chState.readSecondBuf = true # to be flipped later as soon as it enters the main loop
-  chState.waitForWriteFinish::Bool = false # we already did write one buffer
-  status = analogInStatus(hdwf,false); avail,lost,corrupt = analogInStatusRecord(hdwf)
-  @show (avail,lost,corrupt)
-  @inbounds while t < blks*adbufsize-1
-    if avail >= fs_Int32
-      # @show ("1",avail,lost,corrupt)
-      status = analogInStatus(hdwf,true) # read one buffer
-      avail,lost,corrupt = analogInStatusRecord(hdwf)
-      # @show ("2",avail,lost,corrupt)
-      # @show ("UP",t%adbufsize64,chState.waitForWriteFinish,chState.readSecondBuf,chState.writeSecondBuf)
-      if chState.waitForWriteFinish || (chState.writeSecondBuf != chState.readSecondBuf) # avoid writing to the same read buffer
-        for ch in Int32(0):(twoChannels?Int32(chCount-1):Int32(0))
-          # Grab current buffer
-          chdbuf = (ch==0)?ch1dbuf:ch2dbuf
-          writeBuf::Vector{Float64} = chState.writeSecondBuf?chdbuf.chDataBuf2:chdbuf.chDataBuf1
-          analogInStatusData!(writeBuf,hdwf,ch)
-        end
-        chState.writeSecondBuf = !chState.writeSecondBuf
-        # status = analogInStatus(hdwf,false); avail,lost,corrupt = analogInStatusRecord(hdwf)
-        # @show ("3",avail,lost,corrupt)
+  cSamples = 0
+  chDataBuf = Vector{Float64}(adbufsize)
+  cAvailable,cLost,cCorrupted = analogInStatusRecord(hdwf)
+  cSamples += cLost
+  @inbounds for ch in Int32(0):Int32(chCount-1)
+    analogInStatusData!(chDataBuf,hdwf,ch,Int32(cAvailable))
+    # chData[ch+1][cSamples+(1:cAvailable)] = chDataBuf[1:cAvailable]
+    for n in 1:cAvailable
+      if ch==0
+        ch1buf = enque(ch1buf,chDataBuf[n])
+      else
+        ch2buf = enque(ch2buf,chDataBuf[n])
       end
     end
+  end
+  cSamples += cAvailable
 
+  nSamples = blks*adbufsize
+  startT = Libc.time(); t = -1
+  while t < nSamples-1
+    if used(ch1buf) == 0
+      status = analogInStatus(hdwf,true)
+      if (cSamples == 0) && ((status == DWFSTATECONFIG) || (status == DWFSTATEPREFILL) || (status == DWFSTATEARMED))
+        # Acquisition not yet started
+        continue
+      end
+      cAvailable,cLost,cCorrupted = analogInStatusRecord(hdwf)
+      # @show (cSamples,cAvailable,cLost,cCorrupted)
+      cSamples += cLost
+      if cAvailable==0
+        continue
+      end
+      if cSamples+cAvailable > nSamples
+        cAvailable = nSamples-cSamples
+      end
+      # get samples
+      @inbounds for ch in Int32(0):Int32(chCount-1)
+        analogInStatusData!(chDataBuf,hdwf,ch,Int32(cAvailable))
+        # chData[ch+1][cSamples+(1:cAvailable)] = chDataBuf[1:cAvailable]
+        for n in 1:cAvailable
+          if ch==0
+            ch1buf = enque(ch1buf,chDataBuf[n])
+          else
+            ch2buf = enque(ch2buf,chDataBuf[n])
+          end
+        end
+      end
+      cSamples += cAvailable
+    end
+
+    # Now send UDP samples
     currT = Libc.time()
     theorT = t/fs+startT
     if (currT>=theorT)
-      if !chState.waitForWriteFinish
-        t = t+1
-      end
-      tBuf[t+1] = theorT-currT
-      # if t%adbufsize64==0 || t%adbufsize64==1
-      #   @show (t,chState.waitForWriteFinish,chState.readSecondBuf,chState.writeSecondBuf)
-      # end
-      if t%adbufsize64==0 && !chState.waitForWriteFinish
-        chState.readSecondBuf = !chState.readSecondBuf
-        if chState.writeSecondBuf == chState.readSecondBuf
-          chState.waitForWriteFinish = true
+      if used(ch1buf) > 0
+        # @show used(ch1buf)
+        t += 1
+        tBuf[t+1] = theorT-currT
+        for ch in Int32(0):(twoChannels?Int32(chCount-1):Int32(0))
+          if ch==0
+            (voltVal,ch1buf) = deque(ch1buf)
+          else
+            (voltVal,ch2buf) = deque(ch2buf)
+          end
+          valBuf[1] = Float32(voltVal)
+          float32msg!(udpBuf,ch+1,valBuf)
+          Base._send(sock,localh,UInt16(4545),udpBuf) # internal _send is faster and we don't care about failures
         end
       end
-      if chState.writeSecondBuf != chState.readSecondBuf
-        chState.waitForWriteFinish = false
-      end
-      idx = t%adbufsize64+1
-      for ch in Int32(0):(twoChannels?Int32(chCount-1):Int32(0))
-        chdbuf = (ch==0)?ch1dbuf:ch2dbuf
-        readBuf::Vector{Float64} = chState.readSecondBuf?chdbuf.chDataBuf2:chdbuf.chDataBuf1
-        voltVal = readBuf[idx]
-        # chData[ch+1][t+1] = voltVal
-        valBuf[1] = Float32(voltVal)
-        float32msg!(udpBuf,ch+1,valBuf)
-        Base._send(sock,localh,UInt16(4545),udpBuf) # internal _send is faster and we don't care about failures
-      end
-    end
-
-    if (chState.waitForWriteFinish || (theorT-currT > 0.001))# && ((fs-t%adbufsize64 <= fs3_4) || (t%adbufsize64==0))
-      status = analogInStatus(hdwf,false); avail,lost,corrupt = analogInStatusRecord(hdwf)
-      # @show (avail,lost,corrupt)
     end
   end
 
